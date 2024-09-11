8f8c2453e02901808add159ca995b7b58120ca3e
MiikkaJanesaro <122359891+MiikkaJanesaro@users.noreply.github.com>
2024-09-11 21:50:01 +0300

    juu

--- Viidakko.py
+++ Viidakko.py
@@ -2,8 +2,9 @@ import tkinter as tk
 import random
 import time
 
-class Viidakko:
-    def _init_(self,root):
+class ViidakkoSeikkailu:
+    def __init__(self, root):
+        self.root = root
         self.root.title("ernesti ja kernesti viidakossa")
 
         self.viidakko = [[0 for _ in range(100)] for _ in range(100)]
@@ -17,61 +18,61 @@ class Viidakko:
         self.viidakko_text = tk.Text(self.root, height=10, width=50)
         self.viidakko_text.pack()
 
-        def pudota(self):
-            self.ernesti['sijainti'] = (random.randint(0,99), random.randint(0,99))
-            self.kernesti['sijainti'] = (random.randint(0,99), random.randint(0,99))
+    def pudota(self):
+        self.ernesti['sijainti'] = (random.randint(0,99), random.randint(0,99))
+        self.kernesti['sijainti'] = (random.randint(0,99), random.randint(0,99))
 
-            self.upload_viidakko()
+        self.update_viidakko()
 
-            self.move()
+        self.move()
 
-        def update_viidakko(self):
-            self.viidakko = [[0 for _ in range(100)] for _ in range(100)]
+    def update_viidakko(self):
+        self.viidakko = [[0 for _ in range(100)] for _ in range(100)]
 
-            er_sijainti = self.ernesti['sijainti']
-            ker_sijainti = self.kernesti['sijainti']
+        er_sijainti = self.ernesti['sijainti']
+        ker_sijainti = self.kernesti['sijainti']
 
-            self.viidakko[er_sijainti[0]][er_sijainti[1]] = 1
-            self.viidakko[ker_sijainti[0]][ker_sijainti[1]] = 2
+        self.viidakko[er_sijainti[0]][er_sijainti[1]] = 1
+        self.viidakko[ker_sijainti[0]][ker_sijainti[1]] = 2
 
-            self.show_viidakko()
+        self.show_viidakko()
         
-        def show_viidakko():
-            self.viidakko_text.delete(1.0, tk.END)
-            for row in self.viidakko[:10]:
-                self.viidakko_text.insert(tk.END, ' '.join(map(str, row[:10])) + '\n')
+    def show_viidakko(self):
+        self.viidakko_text.delete(1.0, tk.END)
+        for row in self.viidakko[:100]:
+            self.viidakko_text.insert(tk.END, ' '.join(map(str, row[:100])) + '\n')
 
-        def move(self):
+    def move(self):
 
-            while self.ernesti['sijainti'] != self.kernesti['sijainti']:
-
-                self.ernesti['sijainti'] = self.liiku(self.ernesti['sijainti'])
-
-                self.kernesti['sijainti'] = self.liiku(self.kernesti['sijainti'])
+        while self.ernesti['sijainti'] != self.kernesti['sijainti']:
                 
-                self.update_viidakko()
+            self.ernesti['sijainti'] = self.liiku(self.ernesti['sijainti'])
+
+            self.kernesti['sijainti'] = self.liiku(self.kernesti['sijainti'])
                 
-                self.root.update()
-                time.sleep(1)
+            self.update_viidakko()
+                
+            self.root.update()
+            time.sleep(1)
         
-        self.viidakko_text.insert(tk.END, "Vau, onpa mukava nähdä taas!")
+            self.viidakko_text.insert(tk.END, "Vau, onpa mukava nähdä taas!")
 
-        def liiku(self, sijainti):
-            x, y = sijainti
-            suunta = random.choice(['ylös', 'alas', 'vasemmalle', 'oikealle'])
+    def liiku(self, sijainti):
+        x, y = sijainti
+        suunta = random.choice(['ylös', 'alas', 'vasemmalle', 'oikealle'])
             
-            if suunta == 'ylös' and x > 0:
-                x -= 1
-            elif suunta == 'alas' and x < 99:
-                x += 1
-            elif suunta == 'vasemmalle' and y > 0:
-                y -= 1
-            elif suunta == 'oikealle' and y < 99:
-                y += 1
+        if suunta == 'ylös' and x > 0:
+            x -= 1
+        elif suunta == 'alas' and x < 99:
+            x += 1
+        elif suunta == 'vasemmalle' and y > 0:
+            y -= 1
+        elif suunta == 'oikealle' and y < 99:
+            y += 1
             
-            return (x, y)
+        return (x, y)
 
 
 root = tk.Tk()
-peli = ViidakkoPeli(root)
+peli = ViidakkoSeikkailu(root)
 root.mainloop()

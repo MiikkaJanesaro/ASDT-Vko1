3354122d8ba1403a146154d3bb039ce9f8667e78
MiikkaJanesaro <122359891+MiikkaJanesaro@users.noreply.github.com>
2024-09-11 18:20:06 +0300

    Create

---
+++ Viidakko.py
@@ -0,0 +1,77 @@
+import tkinter as tk
+import random
+import time
+
+class Viidakko:
+    def _init_(self,root):
+        self.root.title("ernesti ja kernesti viidakossa")
+
+        self.viidakko = [[0 for _ in range(100)] for _ in range(100)]
+
+        self.ernesti = {"sijainti": (0,0)}
+        self.kernesti = {"sijainti": (0,0)}
+
+        self.button = tk.Button(self.root, text="Pudota Ernesti ja Kernesti", command=self.pudota)
+        self.button.pack()
+
+        self.viidakko_text = tk.Text(self.root, height=10, width=50)
+        self.viidakko_text.pack()
+
+        def pudota(self):
+            self.ernesti['sijainti'] = (random.randint(0,99), random.randint(0,99))
+            self.kernesti['sijainti'] = (random.randint(0,99), random.randint(0,99))
+
+            self.upload_viidakko()
+
+            self.move()
+
+        def update_viidakko(self):
+            self.viidakko = [[0 for _ in range(100)] for _ in range(100)]
+
+            er_sijainti = self.ernesti['sijainti']
+            ker_sijainti = self.kernesti['sijainti']
+
+            self.viidakko[er_sijainti[0]][er_sijainti[1]] = 1
+            self.viidakko[ker_sijainti[0]][ker_sijainti[1]] = 2
+
+            self.show_viidakko()
+        
+        def show_viidakko():
+            self.viidakko_text.delete(1.0, tk.END)
+            for row in self.viidakko[:10]:
+                self.viidakko_text.insert(tk.END, ' '.join(map(str, row[:10])) + '\n')
+
+        def move(self):
+
+            while self.ernesti['sijainti'] != self.kernesti['sijainti']:
+
+                self.ernesti['sijainti'] = self.liiku(self.ernesti['sijainti'])
+
+                self.kernesti['sijainti'] = self.liiku(self.kernesti['sijainti'])
+                
+                self.update_viidakko()
+                
+                self.root.update()
+                time.sleep(1)
+        
+        self.viidakko_text.insert(tk.END, "Vau, onpa mukava nähdä taas!")
+
+        def liiku(self, sijainti):
+            x, y = sijainti
+            suunta = random.choice(['ylös', 'alas', 'vasemmalle', 'oikealle'])
+            
+            if suunta == 'ylös' and x > 0:
+                x -= 1
+            elif suunta == 'alas' and x < 99:
+                x += 1
+            elif suunta == 'vasemmalle' and y > 0:
+                y -= 1
+            elif suunta == 'oikealle' and y < 99:
+                y += 1
+            
+            return (x, y)
+
+
+root = tk.Tk()
+peli = ViidakkoPeli(root)
+root.mainloop()
